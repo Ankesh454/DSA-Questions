1c755079dc116b0ec3b2453bd74c464ad5aeeac9
Ankesh454 <ak4122854@gmail.com>
2026-07-14 09:53:04 +0530

    Resolve merge conflict

--- setMismatch.cpp
+++ setMismatch.cpp
@@ -28,8 +28,8 @@ int main() {
 
     vector<int> ans=mismatch(vec);
     
-    for(int i = 0; i < ans.size(); i++) {
-        cout << ans[i] << " ";
+    for(int i=0;i<ans.size();i++) {
+        cout<<ans[i]<< " ";
     }
     cout<<endl;
     return 0;
