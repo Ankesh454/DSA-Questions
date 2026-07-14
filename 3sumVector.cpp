13bd9b23183bb2c91f724b035dd4f398949c16d0
Ankesh454 <ak4122854@gmail.com>
2026-07-14 10:11:28 +0530

    Add new file

--- 3sumVector.cpp
+++ 3sumVector.cpp
@@ -46,16 +46,11 @@ int main() {
     vector<vector<int>> ans = threeSum(nums);
 
     cout << "Triplets are:\n";
-
-    for (int i = 0; i < ans.size(); i++)
-    {
+    for (int i = 0; i < ans.size(); i++){
         cout << "[ ";
-
-        for (int j = 0; j < ans[i].size(); j++)
-        {
+        for (int j = 0; j < ans[i].size(); j++){
             cout << ans[i][j] << " ";
         }
-
         cout << "]" << endl;
     }
     return 0;
