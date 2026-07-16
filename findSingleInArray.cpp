4c1f98c0dc3a81d0d359452b1594e9882fc05ee0
Ankesh454 <ak4122854@gmail.com>
2026-07-16 15:02:36 +0530

    Add new file

---
+++ findSingleInArray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int findSingle(int nums[],int n){
+    int ans=0;
+
+    for(int i=0;i<n;i++){
+        ans^=nums[i];
+    }
+    return ans;
+}
+
+int main() {
+    int arr[5]={2,4,5,2,4};
+    
+    cout<<findSingle(arr,5);
+    return 0;
+}
