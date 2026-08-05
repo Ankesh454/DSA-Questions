cac75d1bd0d85161edaa8474f50d16dfa99b037a
Ankesh454 <ak4122854@gmail.com>
2026-08-05 11:05:27 +0530

    Add new file

---
+++ findMajority.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+    int n=nums.size();
+    int freq=0,ans=0;
+    for(int i=0;i<n;i++){
+        if(freq==0){
+            ans = nums[i];
+        }
+        if(ans==nums[i]){
+            freq++;
+        }else{
+            freq--;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1,1,1,1,2,3,2,3,2,1,1,2,1};
+    cout<<majorityElement(nums)<<endl;
+    return 0;
+}
