f073d141a2f96508eb4fdfb14e4dfeeb13e5cbeb
Ankesh454 <ak4122854@gmail.com>
2026-09-15 00:32:57 +0530

    Add new file

---
+++ maxAreaInHistogramStack.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+void printArr(vector<int> vec){
+    for (int i=0; i<vec.size(); i++){ 
+        cout<<vec[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+void maxAreaHistogram(vector<int> height){
+    int n = height.size();
+    vector<int> nsl(n); //next smaller left
+    vector<int> nsr(n); //next smaller right
+
+    stack<int> s;
+
+    //next smaller left
+    nsl[0] = -1;
+    s.push(0);
+    for(int i=1;i<n;i++){
+        int curr = height[i];
+        while(!s.empty() && curr <= height[s.top()]){
+           s.pop();
+        }
+        if(s.empty()){
+            nsl[i] = -1;
+        }else{
+            nsl[i] = s.top();
+        }
+        s.push(i);
+    }
+
+    printArr(nsl);
+    while(!s.empty()){
+        s.pop();
+    }
+    //next smaller right
+    s.push(n-1);
+    nsr[n-1] = n;
+
+    for(int i=n-2;i>=0;i--){
+        int curr = height[i];
+        while(!s.empty() && curr <= height[s.top()]){
+            s.pop();
+        }
+        if(s.empty()){
+            nsr[i] = n;
+        }else{
+            nsr[i] = s.top();
+        }
+
+        s.push(i);
+    }
+
+    int maxArea = 0;
+    for(int i=0;i<n;i++){
+        int ht = height[i];
+        int width = nsr[i] - nsl[i] - 1;
+        int area = ht * width;
+
+        maxArea = max(area,maxArea);
+    }
+    cout<<"Max area of histogram : "<<maxArea<<endl;
+}
+
+int main() {
+    vector<int> height = {2,1,5,6,2,3};
+
+    maxAreaHistogram(height);
+
+    return 0;
+}
